2c1384f4d373d8f5dde61960bef65c43ca9a5df5
Lukáš Veškrna <lukas.veskrna@gmail.com>
2025-02-03 23:30:04 +0100

    Improved precision-recall script

--- evaluation/classification_tests.py
+++ evaluation/classification_tests.py
@@ -9,32 +9,30 @@ import sklearn.metrics
 
 from sklearn.metrics import PrecisionRecallDisplay
 
-from sklearn.decomposition import PCA
+#from sklearn.decomposition import PCA
 
 
 EMBEDDINGS = [
-    ["../data/seq_feats.csv", None],
-    ["../data/yeast_emb_only_embeddings.csv", None],
-    ["../data/yeast_emb_embeddings_yeastnet.csv", None],
-    ["../data/yeast_emb_embeddings_genex.csv", None],
-    ["../data/yeast_emb_embeddings_yeastnet_genex.csv", None],
+    ["../data/seq_feats.csv", "Seq. feats."],
+    ["../data/yeast_emb_only_embeddings.csv", "Base"],
+    ["../data/yeast_emb_embeddings_yeastnet.csv", "Base + YeastNet"],
+    ["../data/yeast_emb_embeddings_genex.csv", "Base + Gene ex."],
+    ["../data/yeast_emb_embeddings_yeastnet_genex.csv", "Base + YeastNet + Gene ex."],
 ]
 CLASSIFIERS = [
-    sklearn.ensemble.RandomForestClassifier(100), 
-    sklearn.linear_model.LogisticRegression(max_iter=500)
+    sklearn.linear_model.LogisticRegression(max_iter=1000, n_jobs=8),
+    sklearn.ensemble.RandomForestClassifier(100),
 ]
 
 classes = pd.read_csv("../data/Costanzo_classes.csv").set_index("gene_id")
-print(f"Non-essential genes {(classes == 0).sum().iloc[0]/len(classes)*100:.2f}%")
+essential_ratio = (classes == 0).sum().iloc[0]/len(classes)
+print(f"Non-essential genes {essential_ratio*100:.2f}%")
 
 dataset_names = []
 datasets = []
-for emb_path, proc in EMBEDDINGS:
-    dataset_names.append(emb_path.rstrip(".csv").split("/")[-1])
+for emb_path, emb_name in EMBEDDINGS:
+    dataset_names.append(emb_name)
     emb = pd.read_csv(emb_path, index_col=0).set_index("gene_id")
-
-    if proc is not None:
-        emb = pd.DataFrame(proc.fit_transform(emb), index=emb.index)
 
     dataset = emb.merge(classes, left_index=True, right_index=True)
     X = dataset.iloc[:, :-1]
@@ -42,7 +40,8 @@ for emb_path, proc in EMBEDDINGS:
 
     datasets.append([X, y])
 
-fig, ax = plt.subplots(figsize=(10, 5))
+fig, ax = plt.subplots(figsize=(12, 5))
+ax.set_title("Precision-Recall Curves")
 for i, (name, (X, y)) in enumerate(zip(dataset_names, datasets)):
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
@@ -58,6 +57,11 @@ for i, (name, (X, y)) in enumerate(zip(dataset_names, datasets)):
         y_proba = classifier.predict_proba(X_test)[:, 1]
         PrecisionRecallDisplay.from_predictions(y_test, y_proba, ax=ax, name=f"{classifier} on {name}")
 
+PrecisionRecallDisplay.from_predictions(y_test, np.zeros_like(y_test), ax=ax, name=f"Chance level", linestyle="dotted", color="grey")
+
 ax.legend(bbox_to_anchor=(1, 1))
 plt.tight_layout()
+
+plt.savefig("fig.png", bbox_inches="tight")
+plt.savefig("fig.eps", bbox_inches="tight", format="eps")
 plt.show()
